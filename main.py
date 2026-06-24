f0990eeb4935e40689275079e4b37c99c5d6b550
Rushdi <rushdi.jallad@gmail.com>
2026-06-25 01:22:49 +0300

    B: chapter 1 finishing steps

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import os
+import argparse
 from dotenv import load_dotenv
-from google import genai 
+from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = os.environ.get("GEMINI_API_KEY2")
 if api_key == None: 
     raise ValueError("api key not found")
 
 client = genai.Client(api_key = api_key)
-user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+user_prompt = args.user_prompt
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= user_prompt
-)
+messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
+
+try:
+    
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents= messages
+    )
+except:
+    raise ConnectionError("API called limit error")
+
 if response.usage_metadata is None:
     raise AttributeError("Something went wrong, data is missing!")
 
-
-print(f"User prompt: {user_prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Respone: {response.text}")
+
