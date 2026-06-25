9cf68d947b0def788d8a2b7857e60029820c159c
Rushdi <rushdi.jallad@gmail.com>
2026-06-26 02:50:19 +0300

    H: final version

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.run_python_file import run_python_file
 from functions.write_file import write_file 
+from call_function import call_function
 
 system_prompt = """
 You are a helpful AI coding agent.
@@ -32,7 +33,7 @@ parser.add_argument("--verbose", action="store_true", help="Enable verbose outpu
 args = parser.parse_args()
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY4")
+api_key = os.environ.get("GEMINI_API_KEY1")
 if api_key == None: 
     raise ValueError("api key not found")
 
@@ -43,46 +44,51 @@ messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
 ]
 
-try:
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature = 0)        
-    )
-except:
-    raise ConnectionError("API called limit error")
+for _ in range(5):
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature = 0)        
+        )
+    except:
+        raise ConnectionError("API called limit error")
+    if response.usage_metadata is None:
+        raise AttributeError("Something went wrong, data is missing!")
 
-if response.usage_metadata is None:
-    raise AttributeError("Something went wrong, data is missing!")
-
-if args.verbose:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-if response.function_calls:
-    for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-        kargs = function_call.args 
-        name = function_call.name
-        func = globals()[name] if name else None
-        #from functions.get_files_info import get_files_info
-        #from functions.get_file_content import get_file_content
-        #from functions.run_python_file import run_python_file
-        #from functions.write_file import write_file
-
-        #if name == "get_files_info":
-        #    func = get_files_info
-        #elif name == "get_file_content":
-        #    func = get_file_content
-        #elif name == "run_python_file":
-        #    name = run_python_file
-        #elif name == "write_file":
-        #    func = write_file
-        # 
-        if func and kargs:
-            print(func(".", **kargs))
-        
-else:
-    print(f"Respone: {response.text}")
-
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
+    
+    if response.function_calls:
+        list_functions_result = []
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose=args.verbose)        
+            if ((not function_call_result.parts)
+                or (function_call_result.parts[0].function_response is None)
+                    or (function_call_result.parts[0].function_response.response is None)): raise Exception("function_calls_result")
+            if args.verbose: 
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            list_functions_result.append(function_call_result.parts[0])
+        for fun in list_functions_result:
+            try:
+                messages.append(
+        types.Content(
+            role="function",
+            parts=[
+                types.Part.from_function_response(
+                    name=fun.function_response.name,
+                    response=fun.function_response.response)]))
+            except:
+                pass
+    else:
+        print(f"Respone: {response.text}")
+        exit(0)
+exit(1)
